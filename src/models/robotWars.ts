0a1ca4ab73da53358211caefafd2545dacb0e362
Markus Tacker <Markus.Tacker@NordicSemi.no>
2022-08-12 17:03:48 +0200

    feat: add rubber band holders, expand motor holder spacing

--- src/models/robotWars.ts
+++ src/models/robotWars.ts
@@ -15,9 +15,9 @@ import type {
 import { intersect } from '@jscad/modeling/src/operations/booleans'
 import { hullChain } from '@jscad/modeling/src/operations/hulls'
 import {
+	rotate,
 	rotateY,
 	rotateZ,
-	translateX,
 	translateY,
 	translateZ,
 } from '@jscad/modeling/src/operations/transforms'
@@ -83,10 +83,10 @@ const battery = (thickness: number) =>
 	)
 
 export const motorSupport = (
-	width = 21,
-	thickness = 2,
+	width = 22,
 	depth = 8,
 	height = 18,
+	innerWidth = 18.5,
 ) =>
 	translateZ(
 		height / 2,
@@ -106,38 +106,87 @@ export const motorSupport = (
 						}),
 					),
 					cuboid({
-						size: [depth, 17, width],
+						size: [depth, innerWidth, width],
 					}),
 				),
 				translateZ(
-					9,
+					12,
 					cuboid({
-						size: [depth, width, height / 2],
+						size: [depth, innerWidth, height],
 					}),
 				),
-				translateX(
-					-3.5 / 2,
+				// Hole
+				translate(
+					[-2.5 / 2, 0, 2.5],
 					cuboid({
 						size: [3.5, 25, 5.5],
 					}),
 				),
+
+				// Slant of top cut
+				translate(
+					[-depth / 2, 0, -depth / 2 + 2],
+					rotate(
+						[degToRad(90), degToRad(-45), degToRad(90)],
+						extrudeLinear(
+							{
+								height: depth,
+							},
+							triangle({
+								type: 'AAS',
+								values: [degToRad(90), degToRad(45), width],
+							}),
+						),
+					),
+				),
 			),
-			cuboid({
-				size: [depth, width, thickness],
-				center: [0, 0, -height / 2 + -thickness / 2],
-			}),
 		),
 	)
 
+const rubberStrapHolder = () => {
+	const width = 8.5
+	const depth = 5
+	const thickness = 2
+	const ridges = []
+	for (let i = 0; i < 6; i++) {
+		ridges.push(
+			translate(
+				[0, -2.5, -5 + i * 2],
+				rotate(
+					[degToRad(45), 0, 0],
+					cuboid({
+						size: [50, 2, 2],
+					}),
+				),
+			),
+		)
+	}
+	return translate(
+		[0, 0, 7.5],
+		union(
+			subtract(
+				cuboid({
+					size: [width, depth, 15],
+				}),
+				cuboid({
+					size: [width - thickness * 2, depth - thickness, 15],
+					center: [0, -1, 0],
+				}),
+				...ridges,
+			),
+		),
+	)
+}
+
 export const robotBody = (): (Geom2 | Geom3 | Poly3 | Path2)[] => {
 	const pcbWidth = 63.5
-	const padding = 12
+	const padding = 15
 	const width = pcbWidth + padding * 2
 	const length = 134
 	const thickness = 2
 	const slant = 15
-	const motorSupportWidth = 21
-	const motorSupportDepth = 8
+	const motorSupportWidth = 25
+	const motorSupportDepth = 8.5
 
 	const holes = [
 		[-length + 120, 7.5 + padding],
@@ -150,127 +199,138 @@ export const robotBody = (): (Geom2 | Geom3 | Poly3 | Path2)[] => {
 	] as const
 
 	return [
-		translateZ(
-			2,
+		union(
+			translate(
+				[2, 0, 2],
+				subtract(
+					union(
+						translate(
+							[-16, motorSupportWidth / 2 + 1, 1.5],
+							rotateX(degToRad(slant), motorPlate()),
+						),
+						translate(
+							[-16, width - motorSupportWidth / 2 - 1, 1.5],
+							rotateX(degToRad(-slant), motorPlate()),
+						),
+						translate(
+							[-13 - 44.25, motorSupportWidth / 2, 3],
+							rotateX(
+								degToRad(slant),
+								union(
+									motorSupport(motorSupportWidth, motorSupportDepth),
+									translateZ(
+										-5,
+										cuboid({
+											size: [motorSupportDepth, motorSupportWidth, 10],
+										}),
+									),
+								),
+							),
+						),
+						translate(
+							[-13 - 44.25, width - motorSupportWidth / 2, 3],
+							rotateX(
+								degToRad(-slant),
+								union(
+									motorSupport(motorSupportWidth, motorSupportDepth),
+									translateZ(
+										-5,
+										cuboid({
+											size: [motorSupportDepth, motorSupportWidth, 10],
+										}),
+									),
+								),
+							),
+						),
+						// Add connection plates
+						cuboid({
+							size: [16, 21, thickness],
+							center: [-10, 10.5, -thickness / 2],
+						}),
+						cuboid({
+							size: [16, 21, thickness],
+							center: [-10, width - 10.5, -thickness / 2],
+						}),
+						cuboid({
+							size: [14, motorSupportWidth + 8, thickness],
+							center: [-57, (motorSupportWidth + 8) / 2, -thickness / 2],
+						}),
+						cuboid({
+							size: [14, motorSupportWidth + 8, thickness],
+							center: [
+								-57,
+								width - (motorSupportWidth + 8) / 2,
+								-thickness / 2,
+							],
+						}),
+					),
+					// Clean parts from motor support protruding under plate
+					cuboid({
+						size: [length, width, 10],
+						center: [-length / 2, width / 2, -7],
+					}),
+				),
+			),
+			...holes.map(([x, y]) => screwHoleInHexagon(x, y, thickness)),
 			subtract(
 				union(
-					translate(
-						[-17, motorSupportWidth / 2, 1.5],
-						rotateX(degToRad(slant), motorPlate()),
+					subtract(
+						translateZ(thickness, basePlate(60, width, thickness)),
+						translate(
+							[-2, 2, thickness],
+							basePlate(60 - 2, width - 4, thickness),
+						),
 					),
-					translate(
-						[-17, width - motorSupportWidth / 2, 1.5],
-						rotateX(degToRad(-slant), motorPlate()),
-					),
-					//basePlate(70, width, thickness),
-					translate(
-						[-13 - 37.25, motorSupportWidth / 2, 4.5],
-						rotateX(
-							degToRad(slant),
+					subtract(
+						intersect(
+							// Hexagon
+							translate(
+								[-length * 1.5 - 6, 10, 0],
+								hexagonGrid({
+									cols: 20,
+									rows: 40,
+									height: thickness,
+									size: 8.445,
+									wallThickness: 1,
+								}),
+							),
+							// Cut to shape of frame
 							union(
-								motorSupport(motorSupportWidth, thickness, motorSupportDepth),
-								translateZ(
-									-5,
-									cuboid({
-										size: [motorSupportDepth, motorSupportWidth, 10],
-									}),
-								),
+								translateZ(thickness, basePlate(60, width, thickness)),
+								translateZ(thickness / 2, tip(length, width, thickness)),
 							),
 						),
 					),
-					translate(
-						[-13 - 37.25, width - motorSupportWidth / 2, 4.5],
-						rotateX(
-							degToRad(-slant),
-							union(
-								motorSupport(motorSupportWidth, thickness, motorSupportDepth),
-								translateZ(
-									-5,
-									cuboid({
-										size: [motorSupportDepth, motorSupportWidth, 10],
-									}),
-								),
-							),
-						),
-					),
-					// Add connection plates
-					cuboid({
-						size: [20, 18, thickness],
-						center: [-10, 9, -thickness / 2],
-					}),
-					cuboid({
-						size: [20, 18, thickness],
-						center: [-10, width - 9, -thickness / 2],
-					}),
-					cuboid({
-						size: [14, 24, thickness],
-						center: [-50, 12, -thickness / 2],
-					}),
-					cuboid({
-						size: [14, 24, thickness],
-						center: [-50, width - 12, -thickness / 2],
+					translateZ(thickness / 2, tipFrame(length, width, thickness)),
+				),
+				translate([-12, width / 2, 0], battery(thickness)),
+				// Punch holes
+				...holes.map(([x, y]) =>
+					cylinder({
+						height: thickness,
+						radius: 5,
+						segments: 6,
+						center: [x, y, thickness / 2],
 					}),
 				),
-				// Clean parts from motor support protruding under plate
+			),
+			subtract(
+				scale(
+					[1.1, 1.1, 1],
+					translate([-12, (width * 1) / 1.1 / 2, 0], battery(thickness)),
+				),
+				translate([-12, width / 2, 0], battery(thickness)),
 				cuboid({
-					size: [length, width, 10],
-					center: [-length / 2, width / 2, -7],
+					size: [10, 50, 10],
+					center: [5, width / 2, 0],
 				}),
 			),
-		),
-		...holes.map(([x, y]) => screwHoleInHexagon(x, y, thickness)),
-		subtract(
-			union(
-				subtract(
-					translateZ(thickness, basePlate(60, width, thickness)),
-					translate(
-						[-2, 2, thickness],
-						basePlate(60 - 2, width - 4, thickness),
-					),
-				),
-				subtract(
-					intersect(
-						// Hexagon
-						translateX(
-							-length * 1.5,
-							hexagonGrid({
-								cols: 20,
-								rows: 40,
-								height: thickness,
-								size: 8.445,
-								wallThickness: 1,
-							}),
-						),
-						// Cut to shape of frame
-						union(
-							translateZ(thickness, basePlate(60, width, thickness)),
-							translateZ(thickness / 2, tip(length, width, thickness)),
-						),
-					),
-				),
-				translateZ(thickness / 2, tipFrame(length, width, thickness)),
+			// Holders for rubber straps to fasten battery
+			translate([-11 - 44.25, 29, 2], rubberStrapHolder()),
+			translate(
+				[-11 - 44.25, width - 29, 2],
+				rotateZ(degToRad(180), rubberStrapHolder()),
 			),
-			translate([-11, width / 2, 0], battery(thickness)),
-			// Punch holes
-			...holes.map(([x, y]) =>
-				cylinder({
-					height: thickness,
-					radius: 5,
-					segments: 6,
-					center: [x, y, thickness / 2],
-				}),
-			),
-		),
-		subtract(
-			scale(
-				[1.1, 1.1, 1],
-				translate([-11, (width * 1) / 1.1 / 2, 0], battery(thickness)),
-			),
-			translate([-11, width / 2, 0], battery(thickness)),
-			cuboid({
-				size: [10, 50, 10],
-				center: [5, width / 2, 0],
-			}),
 		),
 	]
 }
@@ -330,7 +390,7 @@ const tipFrame = (length: number, width: number, thickness: number) =>
 			height: thickness,
 			radius: 1,
 			segments: 32,
-			center: [-60, width - 1, 0],
+			center: [-62, width - 1, 0],
 		}),
 		cylinder({
 			height: thickness,
@@ -348,6 +408,6 @@ const tipFrame = (length: number, width: number, thickness: number) =>
 			height: thickness,
 			radius: 1,
 			segments: 32,
-			center: [-60, 1, 0],
+			center: [-62, 1, 0],
 		}),
 	)
